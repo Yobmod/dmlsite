d4128b7c2b46f3dedf8bb442e5afb9362b93599e
Yobmod <yobmod@gmail.com>
2016-09-20 22:05:41 +0100

    Update urls.py
    add form to urls

--- dmlblog/urls.py
+++ dmlblog/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
