3803d5e6b0ad7952bb0d332b53251f30ed6481e1
Yobmod <yobmod@gmail.com>
2016-09-20 22:37:22 +0100

    Update views.py

--- dmlblog/views.py
+++ dmlblog/views.py
@@ -14,13 +14,13 @@ def post_detail(request, pk):
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
-	if form.is_valid():
-		post = form.save(commit=False)
-		post.author = request.user
-		post.published_date = timezone.now()
-		post.save()
-		return redirect('post_detail', pk=post.pk)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm()
-		return render(request, 'dmlblog/post_edit.html', {'form': form})
+	return render(request, 'dmlblog/post_edit.html', {'form': form})
 	
