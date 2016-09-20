81983a246f5be436f8713be3b2c7a0886857c3a2
Yobmod <yobmod@gmail.com>
2016-09-20 22:07:41 +0100

    Update views.py
    update views

--- dmlblog/views.py
+++ dmlblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -9,4 +10,8 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'dmlblog/post_detail.html', {'post': post})
+	
+def post_new(request):
+    form = PostForm()
+    return render(request, 'dmlblog/post_edit.html', {'form': form})
 	
