de70762b366bc750e10113ca6961ae634ba64888
Laventine <yobmod@gmail.com>
2016-09-20 16:02:47 +0100

    added subpages

--- dmlblog/urls.py
+++ dmlblog/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-]
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
