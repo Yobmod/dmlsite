28fdee926ed3d536da779e23ddab02a88bd67a8d
Yobmod <yobmod@gmail.com>
2016-09-20 22:31:20 +0100

    Update views.py
    add if/else to views

--- dmlblog/views.py
+++ dmlblog/views.py
@@ -12,6 +12,15 @@ def post_detail(request, pk):
 	return render(request, 'dmlblog/post_detail.html', {'post': post})
 	
 def post_new(request):
-    form = PostForm()
-    return render(request, 'dmlblog/post_edit.html', {'form': form})
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    	return render(request, 'dmlblog/post_edit.html', {'form': form})
 	
