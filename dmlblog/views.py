8fa1ba3de6f17ec997743d524a8ef86d67490fef
Yobmod <yobmod@gmail.com>
2016-09-20 23:08:12 +0100

    Update views.py

--- dmlblog/views.py
+++ dmlblog/views.py
@@ -36,4 +36,4 @@ def post_edit(request, pk):
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
-	return render(request, 'blog/post_edit.html', {'form': form})
+	return render(request, 'dmlblog/post_edit.html', {'form': form})
