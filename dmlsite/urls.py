0a94558fe1933090e6d60cb9354f50cf1e04819b
Yobmod <yobmod@gmail.com>
2016-09-23 10:41:17 +0100

    Update urls.py
    add login to dmlsite urls

--- dmlsite/urls.py
+++ dmlsite/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+import django.contrib.auth.views
 from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
     url(r'', include('dmlblog.urls')),
 ]
 
