55fb77a774fafc92b1fd197212ea8c266540aa1b
Yobmod <yobmod@gmail.com>
2016-09-23 10:26:36 +0100

    Update views.py
    added login req

--- dmlblog/views.py
+++ dmlblog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,7 +11,8 @@ def post_list(request):
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'dmlblog/post_detail.html', {'post': post})
-	
+
+@login_required	
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
@@ -23,6 +25,7 @@ def post_new(request):
 		form = PostForm()
 	return render(request, 'dmlblog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
@@ -35,16 +38,19 @@ def post_edit(request, pk):
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'dmlblog/post_edit.html', {'form': form})
-	
+
+@login_required
 def post_draft_list(request):
 	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
 	return render(request, 'dmlblog/post_draft_list.html', {'posts': posts})
-	
+
+@login_required	
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=post.pk)
-	
+
+@login_required	
 def post_remove(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	post.delete()
@@ -52,6 +58,7 @@ def post_remove(request, pk):
 
 def contact_admins(request):
 	return render(request, 'dmlblog/contact_admins.html')
-	
+
+@login_required
 def django_admin_page(request):
 	return render(request, 'dmlblog/admin')
