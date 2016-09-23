949616d2a10fd26e832a532eabbb01a05b9a12f7
Yobmod <yobmod@gmail.com>
2016-09-23 14:39:31 +0100

    Update views.py
    add comment buttons

--- dmlblog/views.py
+++ dmlblog/views.py
@@ -75,3 +75,18 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'dmlblog/add_comment_to_post.html', {'form': form})
+    
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('blog.views.post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('blog.views.post_detail', pk=post_pk)
+    
+
