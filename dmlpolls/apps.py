5093662bf2b6cd2e63a31f6fcb88033d57c43f3d
Laventine <yobmod@gmail.com>
2016-09-26 16:16:40 +0100

    start polls app

---
+++ dmlpolls/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DmlpollsConfig(AppConfig):
+    name = 'dmlpolls'
--- dmlsite/urls.py
+++ dmlsite/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
     url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('dmlblog.urls')),
+	url(r'^polls/', include('dmlpolls.urls')),
 ]
 
