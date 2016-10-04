aeabe33847836f5ba103ac6ce226d0a87b3961d5
Laventine <yobmod@gmail.com>
2016-10-04 11:30:43 +0100

    unknown

--- dmlmain/urls.py
+++ dmlmain/urls.py
@@ -8,13 +8,13 @@ import django.contrib.auth.views
 
 
 urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
-       	
+	url(r'^$', views.homepage, name='homepage'),
+
 	url(r'^contact/$', views.contact_admins, name='contact_admins'),
 	url(r'^admin/$', views.django_admin_page, name='django_admin_page'),
 	url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
 	url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
-		
 
-	
+
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
