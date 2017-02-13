4b142f082e723e784f77f2e95b3793f9f0d421c5
Laventine <yobmod@gmail.com>
2017-02-13 14:06:45 +0000

    randomfade <10

--- static_root/dmlmain/js/homepage.js
+++ static_root/dmlmain/js/homepage.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+    $('[data-toggle=tooltip]').tooltip();
+});
+
+$(document).ready(function() {
+	var randNum = Math.floor(Math.random() * 10);
+	if(randNum < 1){
+		$(".randomfade").fadeTo("slow", 1.00);
+		$(".randomfade").fadeTo("slow", 0.00);
+	} else{};
+});
+
+$(document).ready(function() {
 	$("#fadeimg").click(function(){
 		$("#fadeimg").fadeTo(5000, 0.05);
 });
