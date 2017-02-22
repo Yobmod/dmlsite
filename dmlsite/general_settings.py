45bcf10cdceace3748e94cb6740be2e9ca02959a
yobmod <yobmod@gmail.com>
2017-02-22 00:19:48 +0000

    add humanize

--- dmlsite/general_settings.py
+++ dmlsite/general_settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
 	'django.contrib.contenttypes',
 	'django.contrib.sessions',
 	'django.contrib.messages',
+	'django.contrib.humanize',
 	'django.contrib.staticfiles',
 	'dmlblog',
 	'dmlpolls',
