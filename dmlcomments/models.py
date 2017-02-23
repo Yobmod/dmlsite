5b0daac1c94f5ff0c609fc80888ada451434c01d
Laventine <yobmod@gmail.com>
2017-02-23 15:04:38 +0000

    fix heroku migrate

--- dmlcomments/models.py
+++ dmlcomments/models.py
@@ -24,7 +24,7 @@ class Comment(models.Model):
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
 	approved_comment = models.BooleanField(default=False)
-	parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+	parent = models.ForeignKey('Comment', null=True, blank=True)
 
 	objects = CommentManager()
 
