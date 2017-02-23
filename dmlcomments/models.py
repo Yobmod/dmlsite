fb57bf5e94039363c5ebe749a024c2757ba6a73f
Laventine <yobmod@gmail.com>
2017-02-23 15:41:59 +0000

    fix heroku migrate

--- dmlcomments/models.py
+++ dmlcomments/models.py
@@ -17,8 +17,7 @@ class CommentManager(models.Manager):
 
 
 class Comment(models.Model):
-	ContentType = apps.get_model('contenttypes', 'ContentType')
-	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True) #rmv null/blank for new db
+	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE) #rmv null/blank for new db
 	object_id = models.PositiveIntegerField(null=True, blank=True) #rmv null/blank for new db
 	content_object = GenericForeignKey('content_type', 'object_id')
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
