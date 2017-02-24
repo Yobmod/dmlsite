95ff258b3ba02449bfebd1bf3e2712dc80cc49a2
yobmod <yobmod@gmail.com>
2017-02-24 23:38:59 +0000

    add comment div toggle js css

--- static_root/dmlcomments/dmlcomments.js
+++ static_root/dmlcomments/dmlcomments.js
@@ -3,3 +3,17 @@ $(document).ready(function() {
 		$("#commentShow").toggle(1000);
 		});
 });
+
+$(document).ready(function() {
+	$(".replyShowButton").click(function(event){
+		event.preventDefault()
+		$(this).parent().next(".replyShow").toggle(500);
+	});
+});
+
+$(document).ready(function() {
+	$(".addReplyShowButton").click(function(event){
+		event.preventDefault()
+		$(this).parent().next(".addReplyShow").toggle(1000);
+	});
+});
