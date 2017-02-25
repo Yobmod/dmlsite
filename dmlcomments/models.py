986e29f9766a7f65d60b573298dc7409adec5c63
yobmod <yobmod@gmail.com>
2017-02-25 22:52:29 +0000

    ATB 18 done, comment thread form

--- dmlcomments/models.py
+++ dmlcomments/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
 from django.utils import timezone
 
 class CommentManager(models.Manager):
@@ -36,7 +37,7 @@ class Comment(models.Model):
 		return self.text
 
 	def get_absolute_url(self):
-		return reverse("comments:thread", kwargs={"id": self.id})
+		return reverse("comments:comment_thread", kwargs={"pk": self.pk})
 
 	def children(self):
 		return Comment.objects.filter(parent=self)
--- dmlcomments/views.py
+++ dmlcomments/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from dmlcomments.models import Comment
 from dmlcomments.forms import CommentForm
 
 def comment_thread(request, pk):
 	comment = get_object_or_404(Comment, pk=pk)
-	context = {'comment': comment}
+	content_object = comment.content_object #gets poll/post the comment is from
+	content_id = comment.content_object.id
+	initial_data ={"content_type": comment.content_type,
+				   "object_id": comment.object_id,
+				   "author_id": request.user}
+	form = CommentForm(request.POST or None, initial=initial_data)
+	if form.is_valid():
+		c_type = form.cleaned_data.get("content_type")
+		content_type = ContentType.objects.get(model=c_type)
+		obj_id = form.cleaned_data.get('object_id')
+		content_data = form.cleaned_data.get("text")
+		parent_obj = None
+		try:
+			parent_id = int(request.POST.get("parent_id"))
+		except:
+			parent_id = None
+		if parent_id:
+			parent_qs = Comment.objects.filter(id=parent_id)
+			if parent_qs.exists() and parent_qs.count() == 1:
+				parent_obj = parent_qs.first()
+		new_comment, created = Comment.objects.get_or_create(
+							author = request.user,
+							content_type= content_type,
+							object_id = obj_id,
+							text = content_data,
+							parent = parent_obj,			)
+		return HttpResponseRedirect(comment.get_absolute_url())
+	context = {'comment': comment, 'form': form}
 	return render(request, 'comment_thread.html', context)
