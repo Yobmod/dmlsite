758cfaa6bfc6503a6e1fe96e9f2421a625bd7d1e
yobmod <yobmod@gmail.com>
2017-04-05 10:14:21 +0100

    make poster root for conf

--- dmlresearch/urls.py
+++ dmlresearch/urls.py
@@ -11,9 +11,11 @@ from . import views
 
 app_name = 'dmlresearch'
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home'),
-	url(r'^poster/$', views.poster_view, name='poster'),
-	
+    #url(r'^$', HomeView.as_view(), name='home'),
+	#url(r'^poster/$', views.poster_view, name='poster'),
+	url(r'^$', views.poster_view, name='poster'),
+
+
 	#url(r'^restchart/$', HomeView.as_view(), name='home'),
     url(r'^api/data/$', get_data, name='api-data'),
     url(r'^api/chart/data/$', ChartData.as_view()),
