9c0ab51dc456bfab4397c95dbde5399a2fd19e50
Laventine <yobmod@gmail.com>
2017-04-11 12:08:47 +0100

    fix homepage.js

---
+++ static_root/dmlmain/js/homepage.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('[data-toggle=tooltip]').tooltip();
+});
+
+$(document).ready(function() {
+	var randNum = Math.floor(Math.random() * 10);
+	if(randNum < 10){
+		$(".randomfade").fadeTo("slow", 1.00);
+		$(".randomfade").fadeTo("slow", 0.00);
+	} else{};
+});
+
+$(document).ready(function() {
+	$("#fadeimg").click(function(){
+		$("#fadeimg").fadeTo(5000, 0.05);
+});
+});
