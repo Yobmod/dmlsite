6efb83ca429b2505cb2a34c7af0131b54717b4ce
Laventine <yobmod@gmail.com>
2017-08-18 14:46:58 +0100

    add boto

--- dmlsite/production_settings.py
+++ dmlsite/production_settings.py
@@ -39,5 +39,15 @@ DATABASES = {
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-#AWS_ACCESS_KEY_ID =
-#AWS_SECRET_ACCESS_KEY =
+AWS_STORAGE_BUCKET_NAME = 'dmlsite'
+AWS_ACCESS_KEY_ID = 'AKIAIC3XZR7SNQEGZONQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_S3_HOST = 's3.eu-central-1.amazonaws.com'
+
+AWS_S3_OBJECT_PARAMETERS = {
+	 'CacheControl': 'max-age=864000',    #86400 = 1 day
+	 }
