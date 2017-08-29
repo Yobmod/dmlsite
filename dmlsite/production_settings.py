738a7a57119c89332635cf774cb1e2675fb1e58a
Laventine <yobmod@gmail.com>
2017-08-29 14:46:43 +0100

    add bucket to environ vars

--- dmlsite/production_settings.py
+++ dmlsite/production_settings.py
@@ -14,14 +14,13 @@ try:
 except:
 	pass
 
-try:
+
 	EMAIL_HOST = 'smtp.gmail.com'
 	EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 	EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 	EMAIL_PORT = 587
 	EMAIL_USE_TLS = True
-except:
-	pass
+
 
 
 
@@ -45,7 +44,7 @@ DATABASES = {
 DATABASES['default'] = dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
- # AWS_STORAGE_BUCKET_NAME = 'dmlsite'
+ # AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
  # AWS_ACCESS_KEY_ID = os.environ['AWSAccessKeyId']
  # AWS_SECRET_ACCESS_KEY = os.environ['AWSSecretKey']
  # AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
