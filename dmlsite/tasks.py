6eba287eaedb0a23f50dd90346ef13e6a4f8f4fa
yobmod <yobmod@gmail.com>
2017-09-04 18:07:58 +0100

    dj q broke

--- dmlsite/tasks.py
+++ dmlsite/tasks.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django_q.tasks import async
 
 # report generator
-def create_html_report(user):
+def create_html_report():
 	html_report = 'We had a great quarter!'
 	return html_report
 
@@ -13,11 +13,11 @@ def test_hook(task):
 def email_report(task):
 	if task.success:
 		# Email the report
-		async('django.core.mail.send_mail',
-			'The report you requested',
-			task.result,
-			settings.EMAIL_HOST_USER,
-			task.args[0].email)
+		async('django.core.mail.send_mail', #task2 to run
+			'The report you requested', #subject
+			task.result,	#hook, does task2 once task done
+			settings.EMAIL_HOST_USER, #from email
+			'yobmod@yahoo.co.uk') #task.args[0].email) #to email
 	# else:
 	# 	# Tell the admins something went wrong
 	# 	async('django.core.mail.mail_admins',
