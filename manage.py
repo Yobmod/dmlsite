3c7d41d859961e68b48788fa25cdcafe8661d3ac
Laventine <yobmod@gmail.com>
2017-09-04 10:51:41 +0100

    rmv dotenv

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-import dotenv
+#import dotenv
 
 if __name__ == "__main__":
-	dotenv.read_dotenv()
+	#dotenv.read_dotenv()
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dmlsite.settings")
 	try:
 		from django.core.management import execute_from_command_line
