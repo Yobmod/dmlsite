f8080e78649f02c63e010f8d3bb430e26ead01ac
yobmod <yobmod@gemail.com>
2018-04-27 14:26:53 +0100

    change procfile to daphne channels

--- dmlsite/production_settings.py
+++ dmlsite/production_settings.py
@@ -10,15 +10,15 @@ ALLOWED_HOSTS = ['dmlsite.herokuapp.com', "127.0.0.1"]
 SITE_ID = 3
 ROOT_URLCONF = 'dmlsite.urls'
 WSGI_APPLICATION = 'dmlsite.wsgi.application'
-DEBUG = False
+DEBUG = True
 
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_URL'), 'redis://localhost:6379')],
-        },
-    },
+ #   'default': {
+ #       'BACKEND': 'channels_redis.core.RedisChannelLayer',
+  #      'CONFIG': {
+  #          "hosts": [(os.environ.get('REDIS_URL'), 'redis://localhost:6379')],
+ #       },
+   # },
 }
 
 try:
