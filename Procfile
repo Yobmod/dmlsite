b217041721398382e524e83cec157ba1050f6b8f
yobmod <yobmod@gemail.com>
2018-04-27 15:07:56 +0100

    change procfile to daphne channels

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: daphne dmlsite.asgi:application --port $PORT --bind 0.0.0.0 -v2
+web: daphne dmlsite.asgi:application -p $PORT -b 0.0.0.0
 
--- dmlsite/settings.py
+++ dmlsite/settings.py
@@ -1,12 +1,18 @@
 # import os
 # import dj_database_url
+import dotenv
+
+try:
+    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
 from .general_settings import *
 DEBUG = False
 
 try:
-    from .production_settings import *
+    ##from .production_settings import *
 except ImportError:
-    pass
+   # pass
+
 
 try:
     from .local_settings import *
