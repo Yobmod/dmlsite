f749bf17b43784683c9ddebd72ff1179fb35c8bd
yobmod <yobmod@gemail.com>
2018-04-27 14:04:11 +0100

    change procfile to daphne channels

--- dmlsite/asgi.py
+++ dmlsite/asgi.py
@@ -1,14 +1,10 @@
 import os
 import django
 from channels.routing import get_default_application
-from channels.asgi import get_channel_layer
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “dmlsite.settings”)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmlsite.settings')
 django.setup()
-
-
-channel_layer = get_channel_layer()
 
 application = get_default_application()
 application = DjangoWhiteNoise(application)
