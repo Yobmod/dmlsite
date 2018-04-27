14bc2e1d2d636fdfac94c4759a392aeb8da572bb
yobmod <yobmod@gemail.com>
2018-04-27 13:21:35 +0100

    change procfile to daphne channels

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: daphne dmlsite.asgi:application --port $port --bind 0.0.0.0
+web: web: gunicorn dmlsite.wsgi:application
+web2: daphne dmlsite.asgi:application --port $port --bind 0.0.0.0 -v2
 worker: python workers.py
--- dmlsite/asgi.py
+++ dmlsite/asgi.py
@@ -1,7 +1,10 @@
 import os
 import django
 from channels.routing import get_default_application
+from channels.asgi import get_channel_layer
 
 os.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “dmlsite.settings”)
-django.setup()
-application=get_default_application()
+
+
+application = get_default_application()
+channel_layer = get_channel_layer()
