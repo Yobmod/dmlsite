909b72eadc06eb99eaa30d5138029890947ac968
yobmod <yobmod@gemail.com>
2018-04-27 13:55:43 +0100

    change procfile to daphne channels

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn dmlsite.wsgi:application
+web: daphne dmlsite.asgi:application -p 8001
 worker: python manage.py qcluster
--- dmlsite/asgi.py
+++ dmlsite/asgi.py
@@ -2,9 +2,13 @@ import os
 import django
 from channels.routing import get_default_application
 from channels.asgi import get_channel_layer
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault(“DJANGO_SETTINGS_MODULE”, “dmlsite.settings”)
+django.setup()
 
+
+channel_layer = get_channel_layer()
 
 application = get_default_application()
-channel_layer = get_channel_layer()
+application = DjangoWhiteNoise(application)
