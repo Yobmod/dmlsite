26cecccd78ecab2f9deec75b2bd48fd209bf10db
Laventine <laventin@lancaster.ac.uk>
2019-07-17 16:38:50 +0100

    remove whitenoise from asgi

--- dmlsite/asgi.py
+++ dmlsite/asgi.py
@@ -1,10 +1,8 @@
 import os
 import django
 from channels.routing import get_default_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dmlsite.settings')
 django.setup()
 
 application = get_default_application()
-# application = DjangoWhiteNoise(application)
