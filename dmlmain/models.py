79295e5bd7228dc90b0afe11ef7ad5d52b93fa85
yobmod <yobmod@gmail.com>
2019-07-19 21:26:48 +0100

    add types

--- dmlmain/models.py
+++ dmlmain/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-class SignUp(models.Model):
-	email = models.EmailField()
-	name = models.CharField(max_length = 200, blank=True, null=True)
-	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
-	def __str__(self):
-		return self.email
+class SignUp(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=200, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self) -> str:
+        return self.email
